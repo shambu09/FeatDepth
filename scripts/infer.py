787981722adebffc702f0cf8e35185bb7f4188a9
Shambu Valasang <shambuqwerty@gmail.com>
2020-12-20 14:49:54 +0530

    update

--- scripts/infer.py
+++ scripts/infer.py
@@ -18,16 +18,18 @@ from mono.datasets.kitti_dataset import KITTIRAWDataset
 
 cv2.setNumThreads(0)  # This speeds up evaluation 5x on our unix systems (OpenCV 3.3.1)
 
-MIN_DEPTH=1e-3
-MAX_DEPTH=80
-SCALE = 36#we set baseline=0.0015m which is 36 times smaller than the actual value (0.54m)
+MIN_DEPTH = 1e-3
+MAX_DEPTH = 80
+SCALE = 36  # we set baseline=0.0015m which is 36 times smaller than the actual value (0.54m)
+
 
 def transform(cv2_img, height=320, width=1024):
     im_tensor = torch.from_numpy(cv2_img.astype(np.float32)).cuda().unsqueeze(0)
     im_tensor = im_tensor.permute(0, 3, 1, 2).contiguous()
-    im_tensor = torch.nn.functional.interpolate(im_tensor, [height, width],mode='bilinear', align_corners=False)
+    im_tensor = torch.nn.functional.interpolate(im_tensor, [height, width], mode='bilinear', align_corners=False)
     im_tensor /= 255
     return im_tensor
+
 
 def predict(cv2_img, model):
     original_height, original_width = cv2_img.shape[:2]
@@ -40,10 +42,11 @@ def predict(cv2_img, model):
 
     disp = outputs[("disp", 0, 0)]
     disp_resized = torch.nn.functional.interpolate(disp, (original_height, original_width), mode="bilinear", align_corners=False)
-    min_disp = 1/MAX_DEPTH
-    max_disp = 1/MIN_DEPTH
-    depth = 1/(disp_resized.squeeze().cpu().numpy()*max_disp + min_disp) * SCALE
+    min_disp = 1 / MAX_DEPTH
+    max_disp = 1 / MIN_DEPTH
+    depth = 1 / (disp_resized.squeeze().cpu().numpy() * max_disp + min_disp) * SCALE
     return depth, disp_resized.squeeze().cpu().numpy()
+
 
 def evaluate(cfg_path, model_path, img_path, output_path):
     cfg = Config.fromfile(cfg_path)
@@ -69,8 +72,8 @@ def evaluate(cfg_path, model_path, img_path, output_path):
 
 
 if __name__ == "__main__":
-    cfg_path = '../config/cfg_kitti_fm.py'# path to cfg file
-    model_path = '/media/sconly/harddisk/weight/fm_depth.pth'# path to model weight
-    img_path = '../assets/test.png'
-    output_path = '../assets/test_disp.png' # dir for saving depth maps
+    cfg_path = r'..\config\cfg_kitti_fm.py'  # path to cfg file
+    model_path = r'C:\Users\Shambu\Documents\GitHub forked repos\FeatDepth\weights\fm_depth.pth'  # path to model weight
+    img_path = r'..\assets\test.png'
+    output_path = r'..\assets\test_disp.png'  # dir for saving depth maps
     evaluate(cfg_path, model_path, img_path, output_path)
